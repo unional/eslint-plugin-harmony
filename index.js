960c5db4248060e3fa6d7618dacb080388d40dac
Homa Wong <homawong@gmail.com>
2016-03-21 17:15:41 -0700

    Move into its own repo

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'airbnb/base',
+  rules: {
+    // Best Practices
+    complexity: [2, 25],
+
+    // Stylistic Issues
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
+  }
+};
