bee9bb1a303ecd61807195262cc23c13ca095bdf
unional <homawong@gmail.com>
2020-09-05 03:39:12 -0700

    refactor: update style

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import es5Strict from './es5-strict.json';
-import es5 from './es5.json';
-import latest from './latest.json';
-import recommended from './recommended.json';
-import { rules } from './rules';
-import tsPrettier from './ts-prettier.json';
-import tsRecommendedTypeCheck from './ts-recommended-requiring-type-checking.json';
-import tsRecommended from './ts-recommended.json';
+import es5Strict from './es5-strict.json'
+import es5 from './es5.json'
+import latest from './latest.json'
+import recommended from './recommended.json'
+import { rules } from './rules'
+import tsPrettier from './ts-prettier.json'
+import tsRecommendedTypeCheck from './ts-recommended-requiring-type-checking.json'
+import tsRecommended from './ts-recommended.json'
 
 export = {
   configs: {
     es5,
     'es5-strict': es5Strict,
     latest,
-    'recommended': recommended,
+    recommended,
     'ts-prettier': tsPrettier,
     'ts-recommended-requiring-type-checking': tsRecommendedTypeCheck,
     'ts-recommended': tsRecommended,
