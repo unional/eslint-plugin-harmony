5e39d4d6a0fd12aaaca5e25e9e9e4c09d72a9aec
unional <homawong@gmail.com>
2022-05-12 00:57:11 -0700

    fix: change @typescript-eslint/parser as peer
    We are not using it directly.
    It is used in the config.
    Should be a peer so that the right version (and a single version) is installed by the consumer.

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,4 +1,4 @@
-import tsMemberDelimiterStyle from './ts-member-delimiter-style';
+import tsMemberDelimiterStyle from './ts-member-delimiter-style'
 
 export const rules = {
   'ts-member-delimiter-style': tsMemberDelimiterStyle
